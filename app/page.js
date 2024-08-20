bdd751085f6276c99db2aba5ceb27ad1e9e20e41
shishulion <146156073+shishulion@users.noreply.github.com>
2024-08-20 21:27:21 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,15 +2,12 @@
 
 import Link from "next/link";
 import styles from "./page.module.css";
-import Logo from "./ui/Logo";
-import Da from './ui/Da';
 import Hustler from "./ui/Name";
 import {useTelegram} from "./useTg";
 import {useCallback, useEffect, useState, useRef} from "react";
 import Habs from './ui/Art';
 import Auth from './lib/auth';
 import { useRouter } from "next/navigation";
-import Hustle from "./ui/Hustle";
 
 
 
@@ -44,21 +41,14 @@ export default function Home() {
 
   return (
           <main className={styles.main}>
-            <Logo />
+
             {Boolean(!usr) && <p>Ain't work on desktop</p>}
-              <Da />
               <Hustler />
               <br />
             <h1>Cause He Do Art: </h1>
             <br />
-            <Link href="/add"><button className="btn">Создать</button></Link>
-            <div className="penisis"></div>
             <Habs id={user?.id}/>
             <br />
-            <br />
-            <Hustle />
-            <br />
-            <Link href="/about"><b>About project</b></Link>
           </main>
   );
 }
